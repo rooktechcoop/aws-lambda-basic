7c3812294c21170dc67b2463f083785f99b4e9b3
Martin Castro <martincastro.cm@gmail.com>
2020-03-28 20:35:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# aws-lambda-apigw
+An AWS lambda module that
+
+## Usage
+
+```hcl
+module "compute_lambda_test_s3" {
+    providers = {
+        aws.dst       = aws.prod
+    }
+    source  = "../modules/lambda-basic"
+    
+    lambda_function_name            = "${var.namespace}-${var.stage}-lambda-test-s3"
+    lambda_code_path                = "../../lambdas/hello"   
+    lambda_handler                  = "lambda_function.lambda_handler"
+    lambda_runtime                  = "python3.8"
+    lambda_description              = "Example lambda"
+    lambda_policy_arn               = [aws_iam_policy.compute_policy_logs.arn] 
+    lambda_timeout                  = 30
+    tags = {
+        State       = "terraform managed"
+        Environment = "prod"
+    }
+    environment = {
+        variables = {
+            var1        = "value1"
+        }
+    }
+}
+```
+
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:------:|:-----:|
+| lambda\_function\_name| A name for the lambda | string | `-` | yes |
+| lambda\_description| Some description for your lambda | string | `"Some description for your lambda"` | no |
+| lambda\_lambda\_code\_path | The path to your lamda code and packages | string | `-` | yes |
+| lambda\_handler| Lambda handler, e.g: `lambda_function.lambda_handler` | string | `-` | yes |
+| lambda\_runtime| Lambda runtime, e.g: `python3.8` | string | `-` | yes |
+| lambda\_policy\_arn| A list of policie's arn to attach to your lambda role | list(string) | `-` | yes |
+| lambda\_layers| Dictionary of lambda layers arns | list | `null` | no |
+| environment | `aws_lambda_function` environment block, that allows to set variables | object | `null` | no |
+| tags | Tags to attach to your function | map | `null` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| lambda\_arn | ARN of the lambda |
+| lambda\_role\_arn | ARN of the lambda role|
+| lambda\_role\_id | ID of the lambda role|
+| lambda\_role\_name | Name of the lambda role|
+| lambda\_name | Name of the lambda|
+| lambda\_invoke\_uri\_arn | ARN of the lambda invoke uri ARN|
+
+
---
+++ main.tf
@@ -0,0 +1,82 @@
+resource "null_resource" "module_dependency" {
+  triggers = {
+    dependency = var.module_dependency
+  }
+}
+
+data "archive_file" "lambda_zip" {
+
+  type        = "zip"
+  source_dir  = var.lambda_code_path
+  output_path = "${var.lambda_function_name}.zip"
+
+}
+
+resource "aws_lambda_function" "lambda" {
+  depends_on = [null_resource.module_dependency]
+  provider        = aws.dst
+
+  filename         = "${var.lambda_function_name}.zip"
+  source_code_hash = filebase64sha256("${var.lambda_function_name}.zip") #data.archive_file.lambda_zip.output_base64sha256
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+  description      = var.lambda_description
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  tags             = var.tags
+  layers           = var.lambda_layers
+  timeout          = var.lambda_timeout
+
+
+  dynamic "environment" {
+    for_each = var.environment == null ? [] : [var.environment]
+    content {
+      variables = environment.value.variables
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "lambda_cwgroup" {
+  provider    = aws.dst
+
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 14
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  provider    = aws.dst
+  name = "${var.lambda_function_name}-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_iam_role_policy_attachment" {
+  provider    = aws.dst
+  
+  count      = length(var.lambda_policy_arn)
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = var.lambda_policy_arn[count.index] #element(var.lambda_policy_arn, count.index)
+}
+
+resource "null_resource" "module_is_complete" {
+  depends_on = [aws_lambda_function.lambda, aws_cloudwatch_log_group.lambda_cwgroup, aws_iam_role.lambda_role, aws_iam_role_policy_attachment.lambda_iam_role_policy_attachment]
+
+  provisioner "local-exec" {
+    command = "echo Module complete"
+  }
+}
